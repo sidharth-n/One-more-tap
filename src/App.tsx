38f90745fcad014ac9d59da7bc0cbd704e3fc28d
sidharth-n <sidharthnofficial@gmail.com>
2023-10-15 15:56:07 +0530

    add levels to the game

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const maxScreenSize = window.innerHeight;
-  const initialSize = 10;
-  const [size, setSize] = useState(initialSize);
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  const [size, setSize] = useState({ width: 50, height: 50 });
+  const [borderRadius, setBorderRadius] = useState(50);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [level, setLevel] = useState(1);
-  const growthPercentage = 20 - level * 2; // In level 1, it'll grow by 18% of maxScreenSize
-  const shrinkSpeed = level * 0.5; // Higher level, faster shrink
+  const growthPercentage = 20 - level * 2;
+  const [shrinkSpeed, setShrinkSpeed] = useState(0.5);
 
   useEffect(() => {
     let shrinkInterval: NodeJS.Timeout;
 
     if (isGameStarted) {
       shrinkInterval = setInterval(() => {
-        setSize((prevSize) => prevSize - shrinkSpeed);
-        if (size <= 0) {
+        setSize((prevSize) => ({
+          ...prevSize,
+          width: prevSize.width - shrinkSpeed,
+          height: prevSize.height - shrinkSpeed,
+        }));
+        if (size.width <= 0 || size.height <= 0) {
           clearInterval(shrinkInterval);
           setIsGameStarted(false);
-          // handle game over logic here
         }
-      }, 100); // shrink every 100ms
+      }, 100);
     }
 
     return () => {
@@ -33,26 +37,55 @@ const App: React.FC = () => {
     if (!isGameStarted) {
       setIsGameStarted(true);
     }
-    const growth = (maxScreenSize * growthPercentage) / 100;
-    setSize((prevSize) => prevSize + growth);
+    const growth = (screenWidth * growthPercentage) / 100;
 
-    if (size + growth >= maxScreenSize) {
-      setIsGameStarted(false);
-      setLevel((prevLevel) => prevLevel + 1);
-      setSize(initialSize);
+    if (size.width + growth < screenWidth) {
+      setSize((prevSize) => ({
+        width: prevSize.width + growth,
+        height: prevSize.height + growth,
+      }));
+    } else {
+      setSize((prevSize) => ({
+        width: screenWidth,
+        height: prevSize.height + growth,
+      }));
+      setBorderRadius((prevRadius) => Math.max(0, prevRadius - 5));
     }
+
+    if (size.height + growth >= screenHeight && borderRadius === 0) {
+      setIsGameStarted(false);
+    }
+  };
+
+  const handleNextLevel = () => {
+    setLevel((prevLevel) => prevLevel + 1);
+    setSize({ width: 50, height: 50 });
+    setBorderRadius(50);
+    setShrinkSpeed((prevSpeed) => prevSpeed + 0.5); // increase shrink speed for next level
   };
 
   return (
     <>
       <div className="container" onClick={handleTap}>
+        <p className="level-display">Level: {level}</p>
         <div
           className="circle"
-          style={{ width: `${size}px`, height: `${size}px` }}
+          style={{
+            width: `${size.width}px`,
+            height: `${size.height}px`,
+            borderRadius: `${borderRadius}%`,
+          }}
         ></div>
-        {!isGameStarted && <p className="start-text">Tap to start</p>}
-        {size >= maxScreenSize && (
-          <p className="level-done">Level {level} Done!</p>
+        {!isGameStarted && size.width < screenWidth && (
+          <p className="start-text">Tap to start</p>
+        )}
+        {size.height >= screenHeight && borderRadius === 0 && (
+          <>
+            <p className="level-done">Level {level} Completed!</p>
+            <button className="next-level" onClick={handleNextLevel}>
+              Start Level {level + 1}
+            </button>
+          </>
         )}
       </div>
     </>
